12b01429d0a982768f502faeef35249af91366c5
Musenkishi <musenkishi@gmail.com>
2022-01-29 20:59:40 +0100

    Add project files

---
+++ src/types/scraper.ts
@@ -0,0 +1,13 @@
+import { ErrorResult, SuccessResult } from "open-graph-scraper";
+
+export type ScraperResults = (SuccessResult | ErrorResult) & {
+  token: string;
+};
+
+export type ScraperResponse = {
+  headers: ResponseHeaders;
+};
+
+export type ResponseHeaders = {
+  "set-cookie": string[];
+};
