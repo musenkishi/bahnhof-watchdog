2e7259bbcf2d2c1029eff8b1fa1b8f362452896f
musenkishi <musenkishi@gmail.com>
2024-07-04 15:45:04 +0200

    make it env-safe

--- src/app.ts
+++ src/app.ts
@@ -11,63 +11,74 @@ import { handleBuffer } from "./util/buffer"
 
 //Load variables from .env file
 dotenv.config()
+const CRON_SCHEDULE = process.env.CRON_SCHEDULE
 
 const currentSubscription = {
   name: process.env.CURRENT_PRODUCT,
   price: Number.parseInt(process.env.CURRENT_PRICE),
 }
 
-const cronInterval = process.env.CRON_SCHEDULE
-
-if (cronInterval) {
-  cron.schedule(cronInterval, () => {
-    console.log("cron schedule running with interval: " + cronInterval)
-    doPatrol((report) => {
-      sendReport(report)
-    })
-  })
-}
-
 const doPatrol = async (callback: (report: string) => void) => {
   console.log("Watchdog started its patrol.")
 
-  const operationPromise = new Promise<void>((resolve, reject) => {
-    getOperations(process.env.POSTAL_CODE, (operations) => {
-      const currentOutages = operations.data.open
-      const plannedOutages = operations.data.future
-      if (currentOutages.length > 0 || plannedOutages.length > 0) {
-        const message = generateOutageMessage(currentOutages, plannedOutages)
-        callback(message)
-      }
-      resolve()
-    })
-  })
+  const zonePromises = []
 
-  const productsPromise = new Promise<void>((resolve, reject) => {
-    getProducts(process.env.ADDRESS, (result) => {
-      const listedSubscription = getListedSubscription(
-        result.data.products,
-        currentSubscription.name
-      )
+  if (!process.env.POSTAL_CODE) {
+    console.info(
+      "Environment variable POSTAL_CODE missing. Skipping outages check..."
+    )
+  } else {
+    zonePromises.push(
+      new Promise<void>((resolve, reject) => {
+        getOperations(process.env.POSTAL_CODE, (operations) => {
+          const currentOutages = operations.data.open
+          const plannedOutages = operations.data.future
+          if (currentOutages.length > 0 || plannedOutages.length > 0) {
+            const message = generateOutageMessage(
+              currentOutages,
+              plannedOutages
+            )
+            callback(message)
+          }
+          resolve()
+        })
+      })
+    )
+  }
 
-      if (!listedSubscription) {
-        callback(
-          "No subscriptions matches your current subscription or none are available on your address"
-        )
-      }
+  if (!process.env.ADDRESS) {
+    console.info(
+      "Environment variable ADDRESS missing. Skipping subscription price check..."
+    )
+  } else {
+    zonePromises.push(
+      new Promise<void>((resolve, reject) => {
+        getProducts(process.env.ADDRESS, (result) => {
+          const listedSubscription = getListedSubscription(
+            result.data.products,
+            currentSubscription.name
+          )
 
-      if (listedSubscription.price < currentSubscription.price) {
-        const message = generateSubscriptionMessage(
-          currentSubscription,
-          listedSubscription
-        )
-        callback(message)
-      }
-      resolve()
-    })
-  })
+          if (!listedSubscription) {
+            callback(
+              "No subscriptions matches your current subscription or none are available on your address"
+            )
+          }
 
-  await Promise.all([operationPromise, productsPromise])
+          if (listedSubscription.price < currentSubscription.price) {
+            const message = generateSubscriptionMessage(
+              currentSubscription,
+              listedSubscription
+            )
+            callback(message)
+          }
+          resolve()
+        })
+      })
+    )
+  }
+
+  await Promise.all(zonePromises)
 
   console.log("Watchdog has completed its patrol.")
 }
@@ -78,5 +89,19 @@ const sendReport = (report: string) => {
   handleBuffer(report, () => {
     sendWebhook(report)
     sendMail(report)
+  })
+}
+
+if (CRON_SCHEDULE) {
+  cron.schedule(CRON_SCHEDULE, () => {
+    console.log("cron schedule running with interval: " + CRON_SCHEDULE)
+    doPatrol((report) => {
+      sendReport(report)
+    })
+  })
+} else {
+  // no cron schedule set, run patrol once if possible
+  doPatrol((report) => {
+    sendReport(report)
   })
 }
